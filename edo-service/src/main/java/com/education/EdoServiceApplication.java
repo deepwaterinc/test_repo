d65edafe35900847a8c3f3d0ae3ae0e85df4e826
rein_d <room083@gmail.com>
2023-03-31 19:17:56 +0300

    -activate security -some minor changes

--- edo-service/src/main/java/com/education/EdoServiceApplication.java
+++ edo-service/src/main/java/com/education/EdoServiceApplication.java
@@ -2,11 +2,10 @@ package com.education;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.oauth2.client.servlet.OAuth2ClientAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication(exclude = OAuth2ClientAutoConfiguration.class)
+@SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
 public class EdoServiceApplication {
