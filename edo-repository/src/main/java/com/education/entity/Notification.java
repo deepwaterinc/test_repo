ec9df99d5e5578b651b22cb3614443b594dd4a57
Максим Беспалов <maskim.bespalov@domrf.ru>
2023-05-29 22:14:14 +0500

    fix repository start

--- edo-repository/src/main/java/com/education/entity/Notification.java
+++ edo-repository/src/main/java/com/education/entity/Notification.java
@@ -16,13 +16,14 @@ import lombok.experimental.SuperBuilder;
 @AllArgsConstructor
 @ToString
 @SuperBuilder
-@EqualsAndHashCode
 @Table(name = "notification")
 public class Notification extends BaseEntity {
 
     /**
      * Id работника, для которого уведомление
      */
+    @ManyToOne(fetch = FetchType.LAZY, targetEntity = Employee.class)
+    @JoinColumn(name = "employee_id")
     private Employee employee;
 
     /**
